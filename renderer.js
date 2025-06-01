c11fbb36d515f0b81f12e1d45c2014bd1c0da131
Anastasia <ade.nastasia@gmail.com>
2025-06-01 09:04:44 +0300

    v0.1.1 - добавила кнопку Обновить

--- renderer.js
+++ renderer.js
@@ -1,11 +1,18 @@
-fetch('https://api.ipify.org?format=json')
-  .then(response => response.json()) 
-  .then(data => {
-    const ipElement = document.getElementById('ip'); 
-    ipElement.innerText = `Внешний IP хоста: ${data.ip}`; 
-  })
-  .catch(error => {
-    const ipElement = document.getElementById('ip');
-    ipElement.innerText = 'Не удалось получить IP-адрес.';
-    console.error('Ошибка при получении IP:', error);
-  });
+function getIp() {
+  const ipElement = document.getElementById('ip');
+  ipElement.innerText = 'Загрузка IP...';
+
+  fetch('https://api.ipify.org?format=json')
+    .then(response => response.json())
+    .then(data => {
+      ipElement.innerText = `Внешний IP хоста: ${data.ip}`;
+    })
+    .catch(error => {
+      ipElement.innerText = 'Не удалось получить IP-адрес.';
+      console.error('Ошибка при получении IP:', error);
+    });
+}
+
+getIp();
+
+document.getElementById('refreshBtn').addEventListener('click', getIp);
